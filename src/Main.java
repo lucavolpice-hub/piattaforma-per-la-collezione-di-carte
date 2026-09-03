737aa08749f66bb5c1e17568dc849e62c30fe20d
Luca Volpicelli <lukevolpicelli@Mac.station>
2026-09-03 21:27:57 +0200

    tolto out

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,35 @@
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import Gui.FinestraAccesso;
 import controller.ControllerAnnunci;
 import controller.ControllerScambi;
 import controller.ControllerUtenti;
 import controller.Piattaforma;
-
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import model.Annuncio;
 import model.PropostaScambio;
 
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        List<Annuncio> listaAnnunci = new ArrayList<>();
-        List<PropostaScambio> listaProposte = new ArrayList<>();
+        List<Annuncio> listaAnnunci = new ArrayList();
+        List<PropostaScambio> listaProposte = new ArrayList();
         Piattaforma piattaforma = new Piattaforma(listaAnnunci, listaProposte);
-
         ControllerUtenti controllerUtenti = new ControllerUtenti(piattaforma);
-        ControllerAnnunci controllerAnnunci = new ControllerAnnunci(piattaforma, controllerUtenti);
-        ControllerScambi controllerScambi = new ControllerScambi(piattaforma, controllerUtenti);
-
+        new ControllerAnnunci(piattaforma, controllerUtenti);
+        new ControllerScambi(piattaforma, controllerUtenti);
         SwingUtilities.invokeLater(() -> {
             FinestraAccesso finestraAccesso = new FinestraAccesso(controllerUtenti);
-
             JFrame frame = new JFrame("Accesso / Registrazione");
             frame.setContentPane(finestraAccesso.getPanelPrincipale());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(3);
             frame.pack();
-            frame.setLocationRelativeTo(null);
+            frame.setLocationRelativeTo((Component)null);
             frame.setVisible(true);
         });
     }
